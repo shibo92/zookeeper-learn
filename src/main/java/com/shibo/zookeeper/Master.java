c1df766f0ca9bf0f560495969be6fba70726b89d
shibo <shibo@daojia-inc.com>
2020-05-25 11:57:04 +0800

    从节点注册

--- src/main/java/com/shibo/zookeeper/Master.java
+++ src/main/java/com/shibo/zookeeper/Master.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.util.Random;
 
 /**
+ * 主节点
+ *
  * @author by shibo on 2020/5/6.
  */
 public class Master implements Watcher {
@@ -44,13 +46,14 @@ public class Master implements Watcher {
     /**
      * 获取管理权（同步）
      * 通过捕获异常来判断节点状态
+     *
      * @throws KeeperException
      * @throws InterruptedException
      */
     void runForMasterAsync() throws KeeperException, InterruptedException {
         while (true) {
             try {
-                zk.create("/maser", serverId.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+                zk.create("/master", serverId.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
                 isLeader = true;
                 break;
             } catch (KeeperException.NodeExistsException e) {
@@ -58,38 +61,47 @@ public class Master implements Watcher {
                 break;
             } catch (KeeperException.ConnectionLossException e) {
             }
-            if (checkMaster()) {
+            if (checkMasterAsync()) {
                 break;
             }
         }
     }
+
     /**
      * 异步获取管理权限回调方法
      */
     AsyncCallback.StringCallback masterCreateCallback = new AsyncCallback.StringCallback() {
+        @Override
         public void processResult(int rc, String path, Object ctx, String name) {
             switch (KeeperException.Code.get(rc)) {
                 case CONNECTIONLOSS:
                     checkMaster();
-                    return;
+                    break;
                 case OK:
                     isLeader = true;
                     break;
                 default:
                     isLeader = false;
             }
-            System.out.println("I'm " + (isLeader ? "" : "not ") + "the leader.");
+            logger.info("===>>>我 " + (isLeader ? "是" : "不是 ") + " leader.");
         }
     };
+
     /**
      * 获取管理权（异步）
      * 通过回调函数获取节点状态
      */
     void runForMaster() {
-        zk.create("/maser", serverId.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, masterCreateCallback, null);
+        // 临时节点
+        zk.create("/master",
+                serverId.getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL,
+                masterCreateCallback,
+                null);
     }
 
-    boolean checkMaster() {
+    boolean checkMasterAsync() {
         while (true) {
             try {
                 Stat stat = new Stat();
@@ -110,22 +122,65 @@ public class Master implements Watcher {
         }
     }
 
+    void checkMaster() {
+        zk.getData("/master",
+                false,
+                (AsyncCallback.DataCallback) masterCreateCallback,
+                null);
+    }
+
+    @Override
     public void process(WatchedEvent event) {
         System.out.println("===>" + event);
     }
 
+    public void bootstrap() {
+        createParent("/workers", new byte[0]);
+        createParent("/assign", new byte[0]);
+        createParent("/tasks", new byte[0]);
+        createParent("/status", new byte[0]);
+    }
 
+    private void createParent(String path, byte[] data) {
+        // 持久化节点
+        // ctx是为了在回调函数createParentCallback中继续使用
+        zk.create("/path", data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT, createParentCallback, data);
+    }
+
+    AsyncCallback.StringCallback createParentCallback = new AsyncCallback.StringCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, String name) {
+            switch (KeeperException.Code.get(rc)) {
+                case CONNECTIONLOSS:
+                    createParent(path, (byte[]) ctx);
+                    return;
+                case OK:
+                    logger.info("Parent is already registered, path:" + path);
+                    break;
+                default:
+                    logger.error("something wrong:", KeeperException.create(KeeperException.Code.get(rc), path));
+            }
+        }
+    };
 
     public static void main(String[] args) throws Exception {
         Master m = new Master("127.0.0.1:2181");
         m.startZk();
+
+        // m.bootstrap();
+
         m.runForMaster();
+
+        Thread.sleep(1000);
+
+        // m.runForMasterAsync();
         if (m.isLeader) {
             System.out.println("I'm the leader!");
             Thread.sleep(60000);
         } else {
             System.out.println("我不是leader~");
         }
+
         m.stopZK();
     }
 }
---
+++ src/main/java/com/shibo/zookeeper/Worker.java
@@ -0,0 +1,97 @@
+package com.shibo.zookeeper;
+
+import com.sun.jmx.snmp.SnmpUnknownAccContrModelException;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * 从节点
+ *
+ * @author by shibo on 2020/5/21.
+ */
+public class Worker implements Watcher {
+    private static final Logger logger = LoggerFactory.getLogger(Worker.class);
+    private Random random = new Random(this.hashCode());
+    ZooKeeper zk;
+    String hostPort;
+    String serverId = Integer.toHexString(random.nextInt());
+    private String status;
+    private String name
+;
+
+    public Worker(String hostPort) {
+        this.hostPort = hostPort;
+    }
+
+    void startZK() throws IOException {
+        zk = new ZooKeeper(hostPort, 15000, this);
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        logger.error(event.toString() + "," + hostPort);
+    }
+
+    void register() {
+        zk.create("/workers/work-" + serverId,
+                "Idle".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL,
+                createWorkerCallback, null);
+    }
+
+    AsyncCallback.StringCallback createWorkerCallback = new AsyncCallback.StringCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, String name) {
+            switch (KeeperException.Code.get(rc)) {
+                case CONNECTIONLOSS:
+                    register();
+                    return;
+                case OK:
+                    logger.info("register successfully:" + serverId);
+                    break;
+                case NODEEXISTS:
+                    logger.warn("Already registered :" + serverId);
+                    break;
+                default:
+                    // 失败后重新尝试创建
+                    logger.error("something wrong:", KeeperException.create(KeeperException.Code.get(rc), path));
+            }
+        }
+    };
+
+    AsyncCallback.StatCallback statusUpdateCallback = new AsyncCallback.StatCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, Stat stat) {
+            switch (KeeperException.Code.get(rc)) {
+                case CONNECTIONLOSS:
+                    updateStatus((String) ctx);
+                    return;
+                default:
+            }
+        }
+    };
+
+    synchronized private void updateStatus(String status) {
+        if (this.status.equals(status)) {
+            zk.setData("workers/" + name, status.getBytes(), -1, statusUpdateCallback, status);
+        }
+    }
+
+    private void setStatus(String status) {
+        this.status = status;
+        updateStatus(status);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Worker worker = new Worker("9999");
+        worker.startZK();
+        worker.register();
+        Thread.sleep(1000);
+    }
+}
